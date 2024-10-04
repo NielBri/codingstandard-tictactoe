d6c92b688fee4c9a9b249bd668934411e4b6033f
NielBri <briones.nielmatthew@gmail.com>
2024-10-04 11:32:42 +0800

    Added The Player Input section

--- pythonProject/main-tictactoe-program.py
+++ pythonProject/main-tictactoe-program.py
@@ -16,4 +16,20 @@ def print_grid(grid):
     print("  " + grid[3] + " | " + grid[4] + " | " + grid[5])
     print("-------------")
     print("  " + grid[6] + " | " + grid[7] + " | " + grid[8])
-print_grid(grid)
+
+# Detect player input
+def player_input(grid):
+    while True:
+        if player == "X":
+            inp = int(input("Player (X) please choose a number between 1-9 as represented in the grid: "))
+        else:
+            inp = int(input("Player (O) please choose a number between 1-9 as represented in the grid: "))
+        if inp >= 1 and inp <= 9 and grid[inp-1] == "-":
+            grid[inp-1] = player
+            break
+        else:
+            if player == "X":
+                print("Failure. Try agin Player (X)")
+            else:
+                print("Failure. Try agin Player (O)")
+            print_grid(grid)
